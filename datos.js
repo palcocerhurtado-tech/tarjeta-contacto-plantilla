3704d8208d88272605b1de37d51ee2acb3aae907
Pablo Alcocer Hurtado <palcocerhurtado@gmail.com>
2026-04-22 09:05:13 +0200

    Remove personal data from datos.js
    Cleared personal information from datos.js.

--- datos.js
+++ datos.js
@@ -1,17 +1,17 @@
 var datos = {
-  "nombre": "Pablo Alcocer Hurtado",
-  "email": "palcocerhurtado@gmail.com",
-  "telefonoCompleto": "+34693500740",
-  "telefonoMostrado": "+34 693 500 740",
-  "empresa": "Archon consultancies",
-  "puesto": "CEO",
-  "descripcion": "CEO de Archon Consultancies ,Precision , Control , Arquitectura",
-  "web": "https://archonconsultancies.com/",
-  "ubicacion": "Zaragoza, España",
+  "nombre": "",
+  "email": "",
+  "telefonoCompleto": "",
+  "telefonoMostrado": "",
+  "empresa": "",
+  "puesto": "",
+  "descripcion": "",
+  "web": "",
+  "ubicacion": "",
   "redes": {
-    "linkedin": "www.linkedin.com/in/pablo-alcocer-hurtado-a0bb573b6",
-    "github": "https://github.com/palcocerhurtado-tech",
-    "instagram": "@pabloalcoceroficial33"
+    "linkedin": "",
+    "github": "",
+    "instagram": ""
   },
   "diseno": {
     "colorPrincipal": "#999B9B",
